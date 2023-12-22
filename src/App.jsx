db5af766803de6f9505f7e9d7b2ddad86c7143f9
Ryan England <me@rengland.org>
2023-12-21 16:52:39 -0800

    Added carousel navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Image } from 'react-bootstrap';
 import { FaShoppingCart, FaUserAlt } from "react-icons/fa";
 import { IoMdHelp } from "react-icons/io";
+import { MdPlayArrow } from "react-icons/md";
+
 import img from './assets/shopanon.png';
+import busycity from './assets/busycity.jpeg';
+import citypark from './assets/citypark.jpeg';
+import houseparty from './assets/houseparty.jpg';
+
 import ImageCarousel from './components/ImageCarousel';
 
 const App = () => {
+    const images = [busycity, citypark, houseparty];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleImageForward = (e) => {
+    e.stopPropagation();
+    // Increment the current image index
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handleImageBackward = () => {
+    // Decrement the current image index
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <Container>
-        <Container fluid className="d-flex">
+        <Container fluid className="header d-flex">
             <FaUserAlt className="login justify-content-start align-self-center" fill="#FFF"/>
             <Image src={img} alt="logo" className='logo mx-auto justify-content-center' fluid />
             <FaShoppingCart className="cart justify-content-end align-self-center" fill="#FFF"/>
         </Container>
-        <ImageCarousel />
-        <Container fluid className="footer d-flex">
-            <IoMdHelp className='help mx-auto my-4 justify-content-center' fill="#FFF"/>
+        <ImageCarousel {...{ images, currentImageIndex }}/>
+        <Container fluid className="footer d-flex justify-content-center align-items-center" onClick={handleImageBackward}>
+            <MdPlayArrow className="image-backward mx-4" fill="#FFF" />
+            <IoMdHelp className='help mx-3 my-4' fill="#FFF" />
+            <MdPlayArrow className="image-forward mx-4" fill="#FFF" onClick={handleImageForward} />
         </Container>
     </Container>
   );
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Container, Image } from 'react-bootstrap';
-import busycity from '../assets/busycity.jpeg';
-import citypark from '../assets/citypark.jpeg';
-import houseparty from '../assets/houseparty.jpg';
 
-const ImageCarousel = () => {
-    // Static import for prototyping, will be replaced with dynamic import later
-    const images = [busycity, citypark, houseparty];
-    const [currentImage, setCurrentImage] = useState(images[0]);
+const ImageCarousel = ({ images, currentImageIndex }) => {
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const currentIndex = images.indexOf(currentImage);
-            if (currentIndex === images.length - 1) {
-                setCurrentImage(images[0]);
-            } else {
-                setCurrentImage(images[currentIndex + 1]);
-            }
-        }, 15000);
-        return () => clearInterval(interval);
-    }, [currentImage]);
-
-  return (
-    <Container className='carousel text-center'>
-      <Image src={currentImage} alt="carousel" fluid />
-    </Container>
-  );
+    return (
+        <Container className='carousel text-center' style={{ flexDirection: 'column' }}>
+            <Image src={images[currentImageIndex]} alt="carousel" fluid />
+        </Container>
+    );
 };
 
 export default ImageCarousel;
